e787d5b1e7a231a8ad706a4ef72c298714ec3c67
Robert Foerder <robert.foerder@nerdware.dev>
2024-01-02 08:41:36 +0100

    show version number

--- libs/shared/src/environments/environment.prod.ts
+++ libs/shared/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   basePath: 'https://finance-app-function-app.azurewebsites.net/api',
   loginRedirectUri: 'https://eichel-finance.de',
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  appVersion: require('../../package.json').version,
 };
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/services';
 export * from './lib/pipes';
 export * from './lib/shared.module';
+export * from './environments/environment';
