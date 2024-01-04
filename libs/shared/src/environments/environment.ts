94c9c5625f62cb766197ad3776686f1216c09bd4
Robert Foerder <robert.foerder@nerdware.dev>
2024-01-04 06:42:22 +0100

    typo fix

--- libs/shared/src/environments/environment.ts
+++ libs/shared/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   basePath: '//localhost:3000',
   loginRedirectUri: 'http://localhost:4200',
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  appVersion: `v${require('../../../../package.json').version}dev`,
+  appVersion: `${require('../../../../package.json').version}dev`,
 };
 
 import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
